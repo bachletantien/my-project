220ffa51d9a1724f15a5f2a8093b65a3547f785e
Tan Tien <mrbanhu147@gmail.com>
2021-07-13 21:39:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Content from './components/Content';
+import DropDown from './components/DropDown';
 import Footer from './components/Footer';
-import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Home from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Menu from './pages/menu';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resizeeddddd');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Content />
+      <Navbar toggle={toggle} />
+      <DropDown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
+        <Route path='/menu' exact component={Menu} />
+        <Route path='/contact' exact component={Contact} />
+      </Switch>
       <Footer />
     </>
   );
