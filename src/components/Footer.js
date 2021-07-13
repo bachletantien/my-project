75cb51b26faa702f53edc1c80f58a5cc6564af5e
Tan Tien <mrbanhu147@gmail.com>
2021-07-13 18:28:55 +0700

    adding files react

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex justify-center items-center h-16 bg-black text-white'>
+        <p>Copyright ♥ 2021 Egg All right reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
